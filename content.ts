014d3da0933742bf565cda0290ef57fe99ef3392
gritifi <marinaglbova@gmail.com>
2025-05-21 17:45:37 +0300

    Update content.ts

--- content.ts
+++ content.ts
@@ -11,8 +11,8 @@ export const CONTENT = {
         title: 'Let’s work together'
     },
     ABOUT: {
-        about1: 'Hi, I’m Marina — a graphic designer, visual thinker, and the founder of Gritifi. I believe great design starts with structure and ends with emotion. It’s not just about looking good — it’s about feeling right and communicating clearly.',
-        about2: 'I launched Gritifi as a studio where cultural soul meets strategic creativity. My background in graphic design and my roots in a small Bulgarian town shape everything I create — from bold brand identities to thoughtful visual systems. Whether I’m working with a local artisan or an international startup, I bring empathy, clarity, and intention to every project.'
+        about1: 'Hi, I’m Marina — a graphic designer, visual thinker, and the founder of Gritifi. I believe great branding goes beyond visuals. It’s not just about looking good — it’s about feeling right and communicating clearly.',
+        about2: 'We bring empathy, precision, and purpose to every project, with one clear mission: to build bold, strategic brands through smart, intentional design. Working with brands at every stage — providing clarity, creative direction, and expertise in visual identity strategy to build brands that stand out and stand the test of time.'
     },
     FORM: {
         title: 'Ready to turn your vision into something real? Let’s connect and craft a brand that feels just like you...'
